260ec12e6ba560731b7eaa0c493245587345b133
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2021-03-23 07:41:35 -0300

    Set charcters and methods for the game
    This commit adds the heal and attack methods, and also the show status one.

--- app.js
+++ app.js
@@ -4,13 +4,36 @@ function Jugador(nombre){
     this.sp = 100;
 
     this.curar = function(jugadorObjetivo){
-        jugadorObjetivo.hp += 20;
+        
+        if (this.sp >= 40){
+            this.sp -= 40;
+            jugadorObjetivo.hp += 20;
+        }else{
+            console.info(this.nombre + " " + "No posee mas SP")
+        }
+        this.estado(jugadorObjetivo);
+    }
 
+    this.tirarFlecha = function(jugadorObjetivo){
+        if(jugadorObjetivo.hp > 40){
+            jugadorObjetivo.hp -= 40;
+            this.estado(jugadorObjetivo);        
+        }else{
+            jugadorObjetivo.hp = 0;
+            this.estado(jugadorObjetivo);        
+            console.error(jugadorObjetivo.nombre + " " + "Esta muerto");
+        }
+        
+    }
+
+    this.estado = function(jugadorObjetivo){
+        console.info(this);
+        console.info(jugadorObjetivo);
     }
 };
 
 
-var gandalf = new Jugador("Gandalg");
+var gandalf = new Jugador("Gandalf");
 var legolas = new Jugador("Legolas");
 
 gandalf.curar(legolas);
